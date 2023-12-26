7459cd61a09e8b7ed029c8eb53bf5c219252c838
cuu-vnpt <kunnobita97@gmail.com>
2023-12-26 11:30:24 +0700

    new file

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int n; 
+        Console.WriteLine("Nhap kich thuoc mang");
+        n = Convert.ToInt32(Console.ReadLine());
+        int[] array = new int[n + 1];
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine("Nhap cac phan tu mang: ");
+            array[i] = Convert.ToInt32(Console.ReadLine());
+        }
+        Console.WriteLine("Mang co cac phan tu la: ");
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write(array[i]+"\t");
+        }
+        Console.Write("\n");
+        Console.WriteLine("Nhap so can chen: ");
+        int x = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Nhap vi tri can chen: ");
+        int p = Convert.ToInt32(Console.ReadLine());
+        for (int i = 0; i >= p; i--)
+        {
+            array[i] = array[i-1];
+        }
+        array[p-1] =x;
+        for (int i = 0; i <= n; i++)
+        {
+            Console.Write(array[i]+"\t");
+        }
+    }
+}
